1caaa2ae9c4f5eafb7e116d7f4bf95075f1c19ee
becket <becket.bowes@gmail.com>
2022-04-18 20:21:04 -0400

    murican 24hour time/date format because assignment reasons not because its better than UTC

--- model/Thought.js
+++ model/Thought.js
@@ -1,21 +1,25 @@
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../view/utils/dateFormat')
 
 //creates Schema for 'thoughts' and 'reactions' using mongoose
 const ReactionSchema = new Schema({
     reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
     reactionText: { type: String, required: 'you cannot react without an act', maxlength: 280 },
     username: { type: String, required: 'a reaction must have an reactor', },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now, get: (createdAtVal) => dateFormat(createdAtVal) }
+},
+{
+    toJSON: { getters: true }, id: false
 })
 
 const ThoughtSchema = new Schema({
     thoughtText: { type: String, required: 'an empty thought holds no promise', maxlength: 280 },
-    createdAt: { type: Date, default: Date.now },
+    createdAt: { type: Date, default: Date.now, get: (createdAtVal) => dateFormat(createdAtVal) },
     username: { type: String, required: 'a thought must have a thoughter' },
     reactions: [ReactionSchema]
 },
 {
-    toJSON: { virtuals: true }, id: false
+    toJSON: { virtuals: true, getters: true }, id: false
 });
 
 //adds total of reactions as a useable value that isn't stored per se in the database
